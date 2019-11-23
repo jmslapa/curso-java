1368e0675d03387e45bf976bb39444d6de76bb41
jmslapa <lapeta96@gmail.com>
2019-11-23 16:06:06 -0300

    correção na lógica de cálculo da taxa do paypal

--- contract_processing/src/services/PaypalPaymentService.java
+++ contract_processing/src/services/PaypalPaymentService.java
@@ -5,7 +5,7 @@ public class PaypalPaymentService implements PaymentService{
 	public double paymentTax(Double amount, int installmentNumber) {
 		
 		double installmentTax = (double)(installmentNumber * 0.01) * amount;
-		double paymentTax = 0.02 * amount;
+		double paymentTax = 0.02 * (amount + installmentTax);
 		
 		return installmentTax + paymentTax;
 	}
